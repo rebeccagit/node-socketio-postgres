70c67b96e9a9519d131924952633b510a6d0f1b1
rebeccagit <rebeccaa2014@yahoo.com>
2016-01-03 23:19:31 -0600

    whatever

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/aboutme', function(request, response) {
 
 
 
-//database - a wip atm or whatever
+//database - a wip atm or whatever --- :p
 //app.get('/db', function (request, response) {
 //  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 //    client.query('SELECT * FROM test_table', function(err, result) {
