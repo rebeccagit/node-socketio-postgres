db9150edce10d28afeaa2c821a7087a884293de9
rebeccagit <rebeccaa2014@yahoo.com>
2016-01-04 22:59:12 -0600

    Working on chat!

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get('/chat', function(request, response) {
 //  });
 //})
 
-app.http.listen(app.get('port'), function() {
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
