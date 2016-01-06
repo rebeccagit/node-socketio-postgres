caf0cdce630788a67940ec938e3aa600805c211d
rebeccagit <rebeccaa2014@yahoo.com>
2016-01-05 22:05:51 -0600

    Fixing the express server ejs stuff.

--- index.js
+++ index.js
@@ -1,63 +1,26 @@
 var app = require('express')();
-var pg = require('pg');
-var cons = require('consolidate'); // Templating library adapter for Express
-var helmet = require('helmet');
-var favicon = require('serve-favicon');
-
-
-//begin socket server
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//end socket server
 
-
-app.use(app.static('public'));
-app.use(helmet());
-app.use(app.static(__dirname + '/public'));
-app.use(favicon(__dirname + '/public/favicon.ico'));
-
-app.disable('x-powered-by');
-
-app.set('port', (process.env.PORT || 5000));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-
-
-//pages follow
-app.get('/', function(req, res) {
-  res.sendfile('pages/index.ejs');
+app.get('/', function(req, res){
+  res.sendfile('index1.html');
 });
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
-//socket
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-//database - a wip atm or whatever --- :p
-//app.get('/db', function (request, response) {
-//  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//    client.query('SELECT * FROM test_table', function(err, result) {
-//      done();
-//      if (err)
-//       { console.error(err); response.send("Error " + err); }
-//      else
-//       { response.render('pages/db', {results: result.rows} ); }
-//    });
-//  });
-//})
 
-http.listen(app.get('port'), function() {
-  //console.log('Node app is running on port', app.get('port'));
+app.set('port', (process.env.PORT || 5000));
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
 
-//socket
-//http.listen(app.get('port'), function(){
-//  console.log('listening on *:5000');
-//});
 
