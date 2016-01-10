6838cf2a1dd9c32fc33976fd60532faa06b4c218
rebeccagit <rebeccaa2014@yahoo.com>
2016-01-10 17:00:14 -0600

    try again

--- public/myscripts/main.js
+++ public/myscripts/main.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to the Mad Hatter's Chat Room!";
     log(message, {
       prepend: true
     });
