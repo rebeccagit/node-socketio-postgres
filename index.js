0a22c48f2c631bcbac867584e7651282d6ac7326
rebeccagit <rebeccaa2014@yahoo.com>
2016-02-04 02:21:48 -0600

    Adding database change.

--- index.js
+++ index.js
@@ -48,6 +48,10 @@ app.get('/chat', function(request, response) {
   response.render('pages/chat');
 });
 
+app.get('/db', function(request, response) {
+  response.render('pages/db');
+});
+
 
 
 // Chatroom
@@ -114,19 +118,23 @@ io.on('connection', function (socket) {
 
 
 
+var pg = require('pg');
+
+
+
 
 //database - a wip atm
-//app.get('/db', function (request, response) {
-//  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//    client.query('SELECT * FROM test_table', function(err, result) {
-//      done();
-//      if (err)
-//       { console.error(err); response.send("Error " + err); }
-//      else
-//       { response.render('pages/db', {results: result.rows} ); }
-//    });
-//  });
-//})
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM moviereview', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
 http.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
