dcaf68b4b9de8982113a58762608147e31f9f3ff
rebeccagit <rebeccaa2014@yahoo.com>
2016-02-06 10:33:19 -0600

    going back to pg only.

--- index.js
+++ index.js
@@ -134,17 +134,14 @@ pg.connect(connection, function(err, client, done){
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
-
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].number);
-    //output: 1
+  
+  var query = client.query('SELECT review FROM moviereview);
+  
+  query.on('row', function(row) {
+      console.log('Written opinion "%s" and rating %d stars', row.review, row.rating);
+    });
   });
-});
+
 
 
 
