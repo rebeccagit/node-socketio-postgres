330d8a7a412e563cbb9f8fd916ded838a502ec4f
rebeccagit <rebeccaa2014@yahoo.com>
2016-02-05 22:46:12 -0600

    Trying just pg alone.

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
   console.log('Connected to postgres! Getting schemas...');
 
   client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .query('SELECT * FROM moviereviews;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
     });
