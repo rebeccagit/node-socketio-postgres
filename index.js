c081db075b79a84adb74f829e0d4966afa5f8463
rebeccagit <rebeccaa2014@yahoo.com>
2016-02-05 22:59:19 -0600

    Trying just pg alone.

--- index.js
+++ index.js
@@ -135,21 +135,7 @@ var bb = "postgres://ounefajfybheww:e4Wir2p51_lNHwzRYxLdPX54rC@ec2-54-83-199-54.
 
 var dbt = pg.Client(bb);
 
-dbt.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  dbt.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-    dbt.end();
-  });
-});
 
-//database - a wip atm
 
 
 http.listen(app.get('port'), function() {
