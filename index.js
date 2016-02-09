d1fd64af0c84db911985946729ec60459d69f7ba
rebeccagit <rebeccaa2014@yahoo.com>
2016-02-09 08:08:04 -0600

    what the heck.

--- index.js
+++ index.js
@@ -141,6 +141,7 @@ var results = [];
 
 app.get('/db', function(request,response) {
 	//response.render('pages/db');
+	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("My Movie Reviews.  I promise to be unfair, biased and have my own unpredictable, yet chaotic, standards!" + "\n\n\n");
 	var i = 0;
 	var query = db.query("SELECT * FROM moviereview");
