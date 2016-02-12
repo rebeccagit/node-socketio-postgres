f27ed40433fffd2356d9d73f1d155ad952c8ae5d
rebeccagit <rebeccaa2014@yahoo.com>
2016-02-12 03:19:14 -0600

    I don't know what I am doing.

---
+++ database.js
@@ -0,0 +1,20 @@
+function maindb(){
+
+response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("My Movie Reviews.  I promise to be unfair, biased and have my own unpredictable, yet unchaotic, standards!" + "\n\n\n");
+	var i = 0;
+	var query = db.query("SELECT * FROM moviereview");
+		
+		query.on('row', function(row) {
+			console.log(row.name);
+			i++;
+			response.write(String(row.name + row.rating + "   " + row.review) + "\n");
+		});
+		query.on('end', function () {
+			response.write("\nHope you enjoyed!  Number of reviews =" + i + "!");
+			
+			response.end();
+		});
+};
+
+module.exports = maindb;
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 
-//var pg = require('pg');
 var cons = require('consolidate'); // Templating library adapter for Express
 
 var helmet = require('helmet');
@@ -145,21 +144,9 @@ var results = [];
 
 app.get('/db', function(request,response) {
 	//response.render('pages/db');
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("My Movie Reviews.  I promise to be unfair, biased and have my own unpredictable, yet unchaotic, standards!" + "\n\n\n");
-	var i = 0;
-	var query = db.query("SELECT * FROM moviereview");
-		
-		query.on('row', function(row) {
-			console.log(row.name);
-			i++;
-			response.write(String(row.name + row.rating + "   " + row.review) + "\n");
-		});
-		query.on('end', function () {
-			response.write("\nHope you enjoyed!  Number of reviews =" + i + "!");
-			
-			response.end();
-		});
+	
+	maindb();
+	
 });
 
 
