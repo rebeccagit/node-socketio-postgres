64ceb2b087c8be9fe7841cbd4bfd65aeca05485c
rebeccagit <rebeccaa2014@yahoo.com>
2016-06-15 23:25:25 -0500

    roller coaster

--- index.js
+++ index.js
@@ -112,6 +112,9 @@ app.get('/animations7', function(request, response) {
 app.get('/animations8', function(request, response) {
   response.render('pages/animations8');
 });
+app.get('/animations9', function(request, response) {
+  response.render('pages/animations9');
+});
 // practice animation page
 app.get('/og7', function(request, response) {
   response.render('pages/og7');
