f5659363b6c76fa0dc417eb4632ef957773b172d
rebeccagit <rebeccaa2014@yahoo.com>
2016-07-04 20:42:49 -0500

    Hmm.

--- moviesdatabase.js
+++ moviesdatabase.js
@@ -27,7 +27,7 @@ router.get('/', function(req, res) {
 		   
     var results = [];
     var i = 0;
-	var query = db.query("SELECT name, rating, director, actors, video, review FROM moviereviewz ORDER BY name ASC");
+	var query = db.query("SELECT name, rating, director, actors, video, review FROM moviereviewz ORDER BY rating ASC");
 
 	res.write("<ul>");
 		query.on('row', function(row) {
