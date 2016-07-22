346e277955e2d27da76c9b048913598b6d6f0264
rebeccagit <rebeccaa2014@yahoo.com>
2016-07-21 23:48:05 -0500

    doing whatever

--- public/myscripts/sidefile.js
+++ public/myscripts/sidefile.js
@@ -1,10 +1,23 @@
 /* Filename: sidefile.js */
 $(document).ready(function() {
+	
+		$('div').hover(
+				
+		function () {
+			$(this).css({"background-color":"red"});
+        }, 
+				
+        function () {
+            $(this).css({"background-color":"blue"});
+        }
+    );
 
    $("div").click(function() {
-	   
-	  $("*").css("background-color", "yellow");  
-      alert("Hello, world!");
+
+		$("*").css("background-color", "yellow");  
+        alert("Hello, world!");
+	  
+	  
 	  
    });
    
