0c80e943754276d53a2c4a7f57309346c47adfd9
rebeccagit <rebeccaa2014@yahoo.com>
2016-07-24 05:56:37 -0500

    Hmmm

--- public/myscripts/sidefile.js
+++ public/myscripts/sidefile.js
@@ -25,6 +25,10 @@ $(document).ready(function() {
 		//$("*").css("background-color", "white");  
         //alert("Hello, world!");
 	  $(".cat").on("click", aClick).text("Can Click!").css({"background-color" : "yellow"});
+	  
+	  var title = $(".dog").attr("title");
+	  $("#box4").text(title);
+	  
    });
    
 });
