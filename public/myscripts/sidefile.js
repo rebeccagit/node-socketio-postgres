22ad46fedf0d15174a68b3b0fc650599eba29b35
rebeccagit <rebeccaa2014@yahoo.com>
2016-07-24 18:09:34 -0500

    practicing

--- public/myscripts/sidefile.js
+++ public/myscripts/sidefile.js
@@ -1,13 +1,6 @@
 /* Filename: sidefile.js */
 $(document).ready(function() {
-	
-		$("p").css("background-color", "yellow");
-	   
-	    $("#mydiv").addClass("highlight");
-  
-		$(".cat").on("click", aClick).text("Can Click!").css({"background-color" : "yellow"});
-	  
-	
+
 		$("div").hover(
 				
 		function () {
