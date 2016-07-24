552487c4357f49646a9bc00dd4a5dd3909bbc5c8
rebeccagit <rebeccaa2014@yahoo.com>
2016-07-24 17:10:23 -0500

    practicing

--- public/myscripts/sidefile.js
+++ public/myscripts/sidefile.js
@@ -1,19 +1,9 @@
 /* Filename: sidefile.js */
 $(document).ready(function() {
 	
- $("div").click(function() {
-
-		//$("*").css("background-color", "white");  
-        //alert("Hello, world!");
-	  $(".cat").on("click", aClick).text("Can Click!").css({"background-color" : "yellow"});
-	  
-	  var title = $(".dog").attr("title");
-	  $("#box4").text(title);
-	  
-	  $("#mydiv").addClass("highlight");
-	  
-	   });
-   
+	$('p').click(function () {
+		$(this).hide();
+	});
 	
 	
 });
