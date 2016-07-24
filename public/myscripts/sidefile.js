81af70fb82e515ad3e814eac7dc78c9f373ac1ab
rebeccagit <rebeccaa2014@yahoo.com>
2016-07-24 16:53:21 -0500

    practicing

--- public/myscripts/sidefile.js
+++ public/myscripts/sidefile.js
@@ -14,36 +14,4 @@ $(document).ready(function() {
 		);
 	
 	
-
-    function aClick() {
-               $("div").show().fadeIn("slow");
-            };
-
-
-   $("div").click(function() {
-
-		//$("*").css("background-color", "white");  
-        //alert("Hello, world!");
-	  $(".cat").on("click", aClick).text("Can Click!").css({"background-color" : "yellow"});
-	  
-	  var title = $(".dog").attr("title");
-	  $("#box4").text(title);
-	  
-	  $("#mydiv").addClass("highlight");
-	  
-   });
-   
-   $("#driver").click(function(event){
-               $('#box6').load('/myscripts/sidefile.js');
-			   //$getJSON('/myscripts/practice.json', function (resp)
-			   // 	$.each(resp, function ( key, value )
-			   //	alert( key + " : " + value );
-				//	);
-			  // );
-            });
-			
-	$('p').click(function () {
-		$(this).hide();
-	});
-
 });
